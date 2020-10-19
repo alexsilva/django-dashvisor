6bf64eb936c42939d29f6d009d5e8cc3f82fc44e
alex <geniofuturo@gmail.com>
2020-10-19 18:08:37 -0300

    Lazy backend.

--- dashvisor/backends/__init__.py
+++ dashvisor/backends/__init__.py
@@ -1,11 +1,23 @@
+from cached_property import cached_property
 from django.conf import settings
 from django.utils.module_loading import import_string
 
 
-def get_backend():
-    backend_path = getattr(settings, 'DASHVISOR_BACKEND',
-                           'dashvisor.backends.file.Backend')
-    return import_string(backend_path)()
+class Backend:
+    def __init__(self):
+        self.backend_path = getattr(settings, 'DASHVISOR_BACKEND',
+                                    'dashvisor.backends.file.Backend')
+
+    @cached_property
+    def backend(self):
+        return import_string(self.backend_path)()
+
+    def __getattribute__(self, name):
+        get_attribute = object.__getattribute__
+        try:
+            return get_attribute(self, name)
+        except AttributeError:
+            return getattr(get_attribute(self, "backend"), name)
 
 
-backend = get_backend()
+backend = Backend()
